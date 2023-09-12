3856336b0bf266214143d80ce83f76ab235226bc
2SDatDanny <dat-danny.pham@soprasteria.com>
2023-09-12 19:00:24 +0200

    unique name

--- tf/main.tf
+++ tf/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "git_storage" {
-  name                     = "gitteststorage"
+  name                     = "gitteststoraget6y7u89io012e"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
