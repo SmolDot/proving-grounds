3b892163c6aa53332fe5bbf8459e83963e1d8c9f
2SDatDanny <dat-danny.pham@soprasteria.com>
2023-09-12 19:02:09 +0200

    24 tegn

--- tf/main.tf
+++ tf/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "git_storage" {
-  name                     = "gitteststoraget6y7u89io012e"
+  name                     = "gitteststoraget6y7u89io0"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
