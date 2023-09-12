23610644cd583ef101ecc4c4369ce4f7bc1f9ce7
2SDatDanny <dat-danny.pham@soprasteria.com>
2023-09-12 18:16:43 +0200

    client ID

--- tf/providers.tf
+++ tf/providers.tf
@@ -20,6 +20,7 @@ terraform {
     resource_group_name  = "state-access"
     use_oidc             = true
     tenant_id            = "5d557c61-2a5a-4d56-9bdc-37464d75f65b"
+    client_id            = "90e8ee9c-8b65-4622-9836-c1979c103f97"
   }
 
 }
